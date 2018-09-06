f4560143f12e9c7479a48abbfb637bdce561e881
Andrew Gibbs <asg1968@hotmail.co.uk>
2018-09-06 16:45:45 +0100

    Update display when clicking buttons

--- src/interface.js
+++ src/interface.js
@@ -4,18 +4,32 @@ $(document).ready(function() {
 
   $("#plus").click(function(event) {
     thermostat.increase();
+    $("#energyUsage").html(thermostat.currentEnergyUsage());
+    $("#temp_num").html(thermostat.temp);
   });
 
   $("#minus").click(function(event) {
     thermostat.decrease();
+    $("#energyUsage").html(thermostat.currentEnergyUsage());
+    $("#temp_num").html(thermostat.temp);
   });
 
   $("#reset").click(function(event) {
     thermostat.reset();
+    $("#energyUsage").html(thermostat.currentEnergyUsage());
+    $("#temp_num").html(thermostat.temp);
   });
 
   $("#powerSave").click(function(event) {
     thermostat.togglePowerSave();
+    $("#powerSaveDisplay").toggle("fast", function() {
+      $("#powerSaveDisplay").css("color", "lime")
+    });
+
   });
+
+  $("#energyUsage").html(thermostat.currentEnergyUsage());
+
+
 
 });
